21db0aef0f1caee9cd98e7b165a52ac2989ae4e3
sliuxf <31782964+sliuxf@users.noreply.github.com>
2017-11-18 02:24:59 -0500

    tone api

--- routes/tone.js
+++ routes/tone.js
@@ -18,48 +18,19 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/:name', function(req, res, next) {
-    var searchkey = req.params.name;
-    tone_analyzer.tone(searchkey)
-        .then(function (error, response) {
-            res.json(response);
-        }, function(err) {
-            console.error(err);
-        });
-    //console.log(JSON.stringify(response, null, 2));
+    var searchkey = {
+        text: req.params.name,
+        tones: 'emotion'
+    }
+    //var searchkey = req.params.name;
+    tone_analyzer.tone(searchkey, function(error,response) {
+        if (error)
+            console.log('error:', error);
+        else
+            //console.log(response);
+            res.send(response);
 
-
+    });
 });
-
-
-/*
-var params = {
-  // Get the text from the JSON file.
-  text: require('tone.json').text,
-  tones: 'emotion'
-};
-
-tone_analyzer.tone(params, function(error, response) {
-  if (error)
-    console.log('error:', error);
-  else
-    console.log(JSON.stringify(response, null, 2));
-  }
-);
-
-
-
-
-router.get('/:name', function(req, res, next){
-    var searchkey = req.params.name;
-    spotifyApi.searchPlaylists(searchkey)
-        .then(function(data) {
-            res.json(data.body.playlists);
-        }, function(err) {
-            console.error(err);
-        });
-});
-
-*/
-
 
 module.exports = router;
