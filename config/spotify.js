ac7a83795fe280fb2a5bb7a34e44d738a78c3dd4
sliuxf <31782964+sliuxf@users.noreply.github.com>
2017-12-06 19:24:34 -0500

    Update spotify.js

--- config/spotify.js
+++ config/spotify.js
@@ -3,9 +3,9 @@
 //
 
 const SPOTIFY = {
-    clientID: '5b2a4ffeba0049ebb44f42b0042bab84',
-    clientSecret: '89de819dcd284ec090d2f75144b15d35',
-    redirectUri: 'http://localhost:3000/spotify/callback'
+    clientID: '',
+    clientSecret: '',
+    redirectUri: ''
 }
 
 module.exports = SPOTIFY
